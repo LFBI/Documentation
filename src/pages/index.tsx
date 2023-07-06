ac862c574c94a0bc2ea1a6ed9ff9528ce800d357
Romeo Bagunu <rbagunu@lfbi.org>
2023-07-05 22:54:43 -0500

    Conform styles to branding

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import clsx from 'clsx';
 import React from 'react';
-import Link from '@docusaurus/Link';
 
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
@@ -12,16 +11,16 @@ export default function Home(): JSX.Element {
     return(
         <Layout title='Home'>
             <header className={clsx('hero hero--primary', styles.heroBanner)}>
-            <div className="container">
-                <h1 className="hero__title">Living Faith Bible Institute</h1>
-                <p className="hero__subtitle">Student Guidebook</p>
-            </div>
+
+                        <img 
+                        className="d-block img-fluid"
+                        src={require('@site/static/img/lfbi-crest.png').default}
+                        />
+                        <div>
+                        <h1 className="hero__title">Living Faith Bible Institute</h1>
+                        <p className="hero__subtitle">Student Guidebook</p>
+                        </div>
             </header>
-            {/* <Link
-            className="button button--secondary button--lg"
-            to="/docs/category/new-students">
-                    New Student Guide
-            </Link> */}
             <main>
                 <HomepageFeatures />
             </main>
